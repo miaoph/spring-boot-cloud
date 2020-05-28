bc6612113848750c79550bbfca6fa643f23abbbb
miaopenghang <miaopenghang@miaopenghangdeMacBook-Pro.local>
2020-05-28 16:52:51 +0800

    init

---
+++ src/test/java/com/miaoph/cn/springbootcloud/SpringBootCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoph.cn.springbootcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
